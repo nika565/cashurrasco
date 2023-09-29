7be406dc95de91300579cc55583ce2dfa0b533e4
Nathan Barros <nathansousa0809@gmail.com>
2023-09-29 10:04:03 -0300

    Iniciando a tela de calcular churrasco

--- src/screens/calculadora/calculadora.js
+++ src/screens/calculadora/calculadora.js
@@ -1,8 +1,13 @@
-import React from "react";
-import { View, Text, Button } from "react-native"
+import React, { useState } from "react";
+import { View, Text, Button, ScrollView } from "react-native"
+import { CheckBox } from "react-native-elements";
 
-function TelaCalculadora({ route, navigation }){
-    return(
+function TelaCalculadora({ route, navigation }) {
+
+    // State para meu checkbox
+    const [isChecked, setCheked] = useState(false);
+
+    return (
         <View>
             <Text>Calculadora de churras</Text>
             {/* Estes 3 botões abaixo vão ficar no offcanvas */}
@@ -13,7 +18,37 @@ function TelaCalculadora({ route, navigation }){
 
             {/* Esse botão vai ficar no final da página */}
             <Button title="Calcular" onPress={() => navigation.navigate("Resultado")} />
+
+            <CheckBox
+                title="Boi"
+                checkedIcon="check"
+                uncheckedIcon="square-o"
+                checkedColor="orange"
+                checked={isChecked}
+                onPress={() => setCheked(!isChecked)}
+            />
+
+            <CheckBox
+                title="Porco"
+                checkedIcon="check"
+                uncheckedIcon="square-o"
+                checkedColor="orange"
+                checked={isChecked}
+                onPress={() => setCheked(!isChecked)}
+            />
+
+            <CheckBox
+                title="Galinha"
+                checkedIcon="check"
+                uncheckedIcon="square-o"
+                checkedColor="orange"
+                checked={isChecked}
+                onPress={() => setCheked(!isChecked)}
+            />
+
+            <Button title="Teste" onPress={console.log(isChecked)}/>
         </View>
+
     )
 }
 
