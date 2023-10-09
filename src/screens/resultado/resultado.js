bc36b7f56c8bbac39c2dc172d11391b22ddc6394
renato <renatoviturino80@gmail.com>
2023-10-09 13:34:52 -0300

    Arredondando valores

--- src/screens/resultado/resultado.js
+++ src/screens/resultado/resultado.js
@@ -28,19 +28,19 @@ function TelaResultado({ route, navigation }) {
                 <Text style={estilos.tituloCardapio}>Carnes</Text>
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Contra filé</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.contraFile.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.contraFile.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.bovino.contraFile.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Maminha</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.maminha.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.maminha.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.bovino.maminha.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Alcatra</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.alcatra.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.bovino.alcatra.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.bovino.alcatra.precoTotal}</Text> */}
                 </View>
 
@@ -48,19 +48,19 @@ function TelaResultado({ route, navigation }) {
                 {/* CARNE SUINA */}
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Costela</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.costela.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.costela.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.suino.costela.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Filé Suino</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.fileSuino.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.fileSuino.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.suino.fileSuino.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Lombo</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.lombo.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.suino.lombo.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.suino.lombo.precoTotal}</Text> */}
                 </View>
 
@@ -68,19 +68,19 @@ function TelaResultado({ route, navigation }) {
                 {/* LINGUÍÇAS */}
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Toscana</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.toscana.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.toscana.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.linguicas.toscana.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Cuiabana</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.cuiabana.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.cuiabana.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.linguicas.cuiabana.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Linguica de Frango</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.linguicaFrango.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.linguicas.linguicaFrango.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.linguicas.linguicaFrango.precoTotal}</Text> */}
                 </View>
 
@@ -88,19 +88,19 @@ function TelaResultado({ route, navigation }) {
                 {/* CARNE DE FRANGO */}
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Sobrecoxa</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.sobrecoxa.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.sobrecoxa.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.frango.sobrecoxa.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Coração</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.coracao.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.coracao.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.frango.coracao.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Asa</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.asa.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.carnes.frango.asa.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.carnes.frango.asa.precoTotal}</Text> */}
                 </View>
             </View>
@@ -112,25 +112,25 @@ function TelaResultado({ route, navigation }) {
                 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Água</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.agua.qtd} L</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.agua.qtd.toFixed("2")} L</Text>
                     {/* <Text>R${route.params.bebidas.agua.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Refrigerante</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.refri.qtd} L</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.refri.qtd.toFixed("2")} L</Text>
                     {/* <Text>R${route.params.bebidas.refri.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Cerveja</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.cerveja.qtd} L</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.cerveja.qtd.toFixed("2")} L</Text>
                     {/* <Text>R${route.params.bebidas.cerveja.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Suco</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.suco.qtd} L</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.bebidas.suco.qtd.toFixed("2")} L</Text>
                     {/* <Text>R${route.params.bebidas.suco.precoTotal}</Text> */}
                 </View>
             </View>
@@ -148,31 +148,31 @@ function TelaResultado({ route, navigation }) {
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Talheres Descartáveis</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.talheres.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.talheres.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.suprimentos.talheres.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Pratos Descartáveis</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.prato.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.prato.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.suprimentos.prato.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Carvão</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.carvao.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.carvao.qtd.toFixed("0")} Kg</Text>
                     {/* <Text>R${route.params.suprimentos.carvao.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Guardanapos</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.guardanapos.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.guardanapos.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.suprimentos.guardanapos.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Palitos de Dente</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.palitos.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.suprimentos.palitos.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.suprimentos.palitos.precoTotal}</Text> */}
                 </View>
             </View>
@@ -190,31 +190,31 @@ function TelaResultado({ route, navigation }) {
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Farofa</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.farofa.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.farofa.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.acompanhamentos.farofa.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Pão</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.pao.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.pao.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.acompanhamentos.pao.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Pão de Alho</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.paoAlho.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.paoAlho.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.acompanhamentos.paoAlho.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Vinagrete</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.vinagrete.qtd} Kg</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.vinagrete.qtd.toFixed("2")} Kg</Text>
                     {/* <Text>R${route.params.acompanhamentos.vinagrete.precoTotal}</Text> */}
                 </View>
 
                 <View style={estilos.linha}>
                     <Text style={[estilos.nomeCarne, estilos.corCinza]}>Queijo Coalho</Text>
-                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.queijoCoalho.qtd} UN</Text>
+                    <Text style={[estilos.kilos, estilos.corCinza]}>{route.params.acompanhamentos.queijoCoalho.qtd.toFixed("0")} UN</Text>
                     {/* <Text>R${route.params.acompanhamentos.queijoCoalho.precoTotal}</Text> */}
                 </View>
             </View>
